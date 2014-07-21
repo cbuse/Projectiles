92a4fb8c9b403bde5f28c38b92facad452c1311e
cbuse <cristina_buse@yahoo.com>
2014-07-21 22:28:49 +0300

    Create TimeManager.java

---
+++ ProcessingManagers/TimeManager.java
@@ -0,0 +1,30 @@
+package ProcessingManagers;
+
+/**
+ * Time object
+ */
+public class TimeManager {
+	private int h,m,s;
+	
+	public TimeManager(int h, int m, int s){
+		this.h = h;
+		this.m = m;
+		this.s =s;
+	}
+	
+	public int getH(){
+		return h;
+	}
+	
+	public int getM(){
+		return m;
+	}
+	
+	public int getS(){
+		return s;
+	}
+
+	// TODO => implement the body of the class (set class fields, make ways 
+	// 			for other classes to work with a TimeManager object)
+	
+}
